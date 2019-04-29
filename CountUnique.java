8678c11b37551ede98f610e1ad63adb4c3eb7d2a
Liyun Cecil <liyuncecil@gmail.com>
2019-04-29 09:29:58 -0700

    Assignment 3: countUnique

---
+++ CountUnique.java
@@ -0,0 +1,39 @@
+import java.io.File;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class CountUnique {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		List<Integer> list = new LinkedList<Integer>();
+		list.add(1);
+		list.add(3);
+		list.add(5);
+		list.add(3);
+		list.add(5);
+		list.add(6);
+		
+		System.out.println(countUnique(list));
+		
+		
+		
+	}
+	
+	
+	public static int countUnique(List<Integer> list) {
+	    
+		//create a set to store the list of numbers
+	    Set<Integer> numbers = new TreeSet<Integer>(list);
+	    
+	    return numbers.size();
+		
+	}
+		
+}
