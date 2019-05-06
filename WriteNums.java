121b7456d5f5d0871b44972030f798ddae0b6b16
Liyun Cecil <liyuncecil@gmail.com>
2019-05-05 19:04:18 -0700

    Assignment 5: writeNums()

---
+++ WriteNums.java
@@ -0,0 +1,31 @@
+
+public class WriteNums {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		writeNums(12);
+	}
+
+	public static void writeNums(int n) {
+		
+		if(n < 1) {
+			
+			throw new IllegalArgumentException();
+			
+		}else if(n == 1) {
+			
+			System.out.print(1);
+			
+		}else {
+			
+			writeNums(n-1);
+			System.out.print(", " + n);
+			
+			
+			
+			
+			
+			
+		}
+	}
+}
